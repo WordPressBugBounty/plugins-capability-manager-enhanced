db93a46c291a0cff0612e1c24f4bf9a89df5b51f
Sean Murphy <sean@iamseanmurphy.com>
2026-06-11 15:23:11 +0000

    2.45.0

--- capability-manager-enhanced/lib/vendor/composer/installed.php
+++ capability-manager-enhanced/lib/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'v2.44.0',
-        'version' => '2.44.0.0',
-        'reference' => '44afe5d17bbf4f51a8539293f4034c9dcb415131',
+        'pretty_version' => 'v2.45.0',
+        'version' => '2.45.0.0',
+        'reference' => '0e06b0641b0bd7b479abe3cba2904dba7acb61b0',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'v2.44.0',
-            'version' => '2.44.0.0',
-            'reference' => '44afe5d17bbf4f51a8539293f4034c9dcb415131',
+            'pretty_version' => 'v2.45.0',
+            'version' => '2.45.0.0',
+            'reference' => '0e06b0641b0bd7b479abe3cba2904dba7acb61b0',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
